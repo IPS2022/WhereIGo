e08dcddbd98a527540ebbbe409fe8e2e04086e0d
Kyungmin Lee <min2000.lee@gmail.com>
2022-05-06 10:56:51 +0900

    로그아웃 기능 완성
    my pagfe에 로그아웃 버튼 존재

--- WhereIGo/app/src/main/java/com/example/whereigo/FragmentMypage.java
+++ WhereIGo/app/src/main/java/com/example/whereigo/FragmentMypage.java
@@ -1,17 +1,35 @@
 package com.example.whereigo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
-    public class FragmentMypage extends Fragment {
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
+
+public class FragmentMypage extends Fragment {
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_mypage, container, false);
+
+            Button logoutBtn=(Button)rootView.findViewById(R.id.mypage_logout_btn);
+
+            logoutBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    FirebaseAuth.getInstance().signOut();
+                    Toast.makeText(getContext(), "sign out", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                    startActivity(intent);
+                }
+            });
 
             return rootView;
         }
--- WhereIGo/app/src/main/java/com/example/whereigo/MainActivity.java
+++ WhereIGo/app/src/main/java/com/example/whereigo/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.whereigo;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -21,12 +23,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.menu_frame_layout, fragmentHome).commitAllowingStateLoss();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.menu_bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+
 
     }
 
